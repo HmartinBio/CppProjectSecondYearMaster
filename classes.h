7ce0772ff48a053ed3bee9b1f925783d7e9b0b07
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-09-16 15:27:37 +0200

    Adding headers of classes

---
+++ classes.h
@@ -0,0 +1,51 @@
+#ifndef CLASSES_HEADER
+#define CLASSES_HEADER
+
+namespace project{
+    class Clan{
+        public:
+
+        private:
+
+
+    }
+
+}
+    class Nature{
+        public:
+
+        private:
+
+    }
+
+    class Attributs{
+        public:
+
+        private:
+    }
+
+    class Abilities{
+        public:
+
+        private:
+    }
+
+    class Avantages{
+        public:
+
+        private:
+    }
+
+    class Controleur{
+        public:
+
+        private:
+    }
+
+    class Merits_flows{
+        public:
+
+        private:
+    }
+
+#endif
