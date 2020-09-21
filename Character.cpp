a4b4bb174e11d7baff7252833569908f0bc818f1
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-09-21 19:41:39 +0200

    Update source code of Character class

--- Character.cpp
+++ Character.cpp
@@ -1,4 +1,32 @@
 #include "classes.h"
 
-vampireGameproject::Character::Character();
-void vampireGameproject::Character::setNature
+vampireGameproject::Character::Character(std::string nature, char playerName, std::string chronicle, int age, std::string demeanor, std::string clan, std::string generation){
+    Character::playerName = playerName;
+    setNature(nature);
+    setChronicle(chronicle);
+    setAge(age);
+    setDemeanor(demeanor);
+    setClan(clan);
+    setGeneration(generation);
+}
+
+void vampireGameproject::Character::setNature(std::string nature){
+    Character::nature = nature;
+}
+void vampireGameproject::Character::setChronicle(std::string chronicle){
+    Character::chronicle = chronicle;
+}
+void vampireGameproject::Character::setDemeanor(std::string demeanor){
+    Character::demeanor = demeanor;
+}
+void vampireGameproject::Character::setClan(std::string clan){
+    Character::clan = clan;
+}
+
+void vampireGameproject::Character::setGeneration(std::string generation){
+    Character::generation = generation;
+}
+
+void vampireGameproject::Character::setAge(int age){
+    Character::age = age;
+}
