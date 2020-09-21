209e27c5f8db766bedf532fc4e5591f9cb75e582
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-09-21 22:05:02 +0200

    Adding source code for Attributes class

---
+++ Attributes.cpp
@@ -0,0 +1,26 @@
+#include "classes.h"
+
+vampireGameproject::Attributes::Attributes(){
+    physicalAttributes.insert(std::pair<char,int>("Strength", 0));
+    physicalAttributes.insert(std::pair<char,int>("Dexterity", 0));
+    physicalAttributes.insert(std::pair<char,int>("Stamina", 0));
+    socialAttributes.insert(std::pair<char,int>("Charisma",0));
+    socialAttributes.insert(std::pair<char,int>("Manipulation",0));
+    socialAttributes.insert(std::pair<char,int>("Appearance",0));
+    mentalAttributes.insert(std::pair<char,int>("Perception",0));
+    mentalAttributes.insert(std::pair<char,int>("Intelligence",0));
+    mentalAttributes.insert(std::pair<char,int>("Wits",0)); 
+};
+
+std::map<char,int> vampireGameproject::Attributes::getAttributes(void){}
+
+void vampireGameproject::Attributes::setAttributes(char tableName,char attributeName, int value){
+    swith(tableName){
+        case 'physical': physicalAttributes[attributeName] += value;
+            break;
+        case 'social': socialAttributes[attributeName] += value;
+            break;
+        case 'mental': mentalAttributes[attributeName] += value;
+            break; 
+    }
+}
