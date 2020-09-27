d646bf606d39e345e230785e733336abf256e462
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-09-27 19:46:42 +0200

    Modifing source code for the Abilities class

--- Abilities.cpp
+++ Abilities.cpp
@@ -1,39 +1,39 @@
 #include "classes.h"
 
 vampireGameproject::Abilities::Abilities(){
-    talents.insert(std::pair<char,int>("Alertness", 0));
-    talents.insert(std::pair<char,int>("Athletics", 0));
-    talents.insert(std::pair<char,int>("Awareness", 0));
-    talents.insert(std::pair<char,int>("Brawl", 0));
-    talents.insert(std::pair<char,int>("Empathy", 0));
-    talents.insert(std::pair<char,int>("Expression", 0));
-    talents.insert(std::pair<char,int>("Intimidation", 0));
-    talents.insert(std::pair<char,int>("Leadership", 0));
-    talents.insert(std::pair<char,int>("Streetwise", 0));
-    talents.insert(std::pair<char,int>("Subterfuge", 0));
-    talents.insert(std::pair<char,int>("Alertness", 0));
-    skills.insert(std::pair<char,int>("Animal Ken", 0));
-    skills.insert(std::pair<char,int>("Crafts", 0));
-    skills.insert(std::pair<char,int>("Drive", 0));
-    skills.insert(std::pair<char,int>("Etiquette", 0));
-    skills.insert(std::pair<char,int>("Firearms", 0));
-    skills.insert(std::pair<char,int>("Larceny", 0));
-    skills.insert(std::pair<char,int>("Melee", 0));
-    skills.insert(std::pair<char,int>("Performance", 0));
-    skills.insert(std::pair<char,int>("Stealth", 0));
-    skills.insert(std::pair<char,int>("Survival", 0));
-    skills.insert(std::pair<char,int>("Professional Skill", 0));
-    knowledges.insert(std::pair<char,int>("Academics", 0));
-    knowledges.insert(std::pair<char,int>("Computer", 0));
-    knowledges.insert(std::pair<char,int>("Finance", 0));
-    knowledges.insert(std::pair<char,int>("Investigation", 0));
-    knowledges.insert(std::pair<char,int>("Law", 0));
-    knowledges.insert(std::pair<char,int>("Medicine", 0));
-    knowledges.insert(std::pair<char,int>("Occult", 0));
-    knowledges.insert(std::pair<char,int>("Politics", 0));
-    knowledges.insert(std::pair<char,int>("Science", 0));
-    knowledges.insert(std::pair<char,int>("Technology", 0));
-    knowledges.insert(std::pair<char,int>("Expert Knowledge", 0));
+    talents.insert(std::pair<std::string,int>("Alertness", 0));
+    talents.insert(std::pair<std::string,int>("Athletics", 0));
+    talents.insert(std::pair<std::string,int>("Awareness", 0));
+    talents.insert(std::pair<std::string,int>("Brawl", 0));
+    talents.insert(std::pair<std::string, int>("Empathy", 0));
+    talents.insert(std::pair<std::string,int>("Expression", 0));
+    talents.insert(std::pair<std::string,int>("Intimidation", 0));
+    talents.insert(std::pair<std::string,int>("Leadership", 0));
+    talents.insert(std::pair<std::string,int>("Streetwise", 0));
+    talents.insert(std::pair<std::string,int>("Subterfuge", 0));
+    talents.insert(std::pair<std::string,int>("Alertness", 0));
+    skills.insert(std::pair<std::string,int>("Animal Ken", 0));
+    skills.insert(std::pair<std::string,int>("Crafts", 0));
+    skills.insert(std::pair<std::string,int>("Drive", 0));
+    skills.insert(std::pair<std::string,int>("Etiquette", 0));
+    skills.insert(std::pair<std::string,int>("Firearms", 0));
+    skills.insert(std::pair<std::string,int>("Larceny", 0));
+    skills.insert(std::pair<std::string,int>("Melee", 0));
+    skills.insert(std::pair<std::string,int>("Performance", 0));
+    skills.insert(std::pair<std::string,int>("Stealth", 0));
+    skills.insert(std::pair<std::string,int>("Survival", 0));
+    skills.insert(std::pair<std::string,int>("Professional Skill", 0));
+    knowledges.insert(std::pair<std::string,int>("Academics", 0));
+    knowledges.insert(std::pair<std::string,int>("Computer", 0));
+    knowledges.insert(std::pair<std::string,int>("Finance", 0));
+    knowledges.insert(std::pair<std::string,int>("Investigation", 0));
+    knowledges.insert(std::pair<std::string,int>("Law", 0));
+    knowledges.insert(std::pair<std::string,int>("Medicine", 0));
+    knowledges.insert(std::pair<std::string,int>("Occult", 0));
+    knowledges.insert(std::pair<std::string,int>("Politics", 0));
+    knowledges.insert(std::pair<std::string,int>("Science", 0));
+    knowledges.insert(std::pair<std::string,int>("Technology", 0));
+    knowledges.insert(std::pair<std::string,int>("Expert Knowledge", 0));
 }
 
 std::map<char,int> vampireGameproject::Abilities::getAbilities(char tableName){
