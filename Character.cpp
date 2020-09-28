1e989e7b15a845f83eed52deeca13393c3602aee
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-09-28 22:12:00 +0200

    Modifing source code of the Character class

--- Character.cpp
+++ Character.cpp
@@ -1,40 +1,17 @@
 #include "classes.h"
 
-vampireGameproject::Character::Character(std::string nature, char playerName, std::string chronicle, int age, std::string demeanor, std::string clan, std::string generation){
-    Character::playerName = playerName;
-    setNature(nature);
-    setChronicle(chronicle);
-    setAge(age);
-    setDemeanor(demeanor);
-    setClan(clan);
-    setGeneration(generation);
+vampireGameproject::Character::Character(Parsetext& textParser){
+    characterDaemenor()
+    
 }
 
-void vampireGameproject::Character::setNature(std::string nature){
-    Character::nature = nature;
-}
-void vampireGameproject::Character::setChronicle(std::string chronicle){
-    Character::chronicle = chronicle;
-}
-void vampireGameproject::Character::setDemeanor(std::string demeanor){
-    Character::demeanor = demeanor;
-}
-void vampireGameproject::Character::setClan(std::string clan){
-    Character::clan = clan;
+vampireGameproject::Character::~Character(){}
+
+std::string vampireGameproject::Character::getPlayername(void){
+    return playerName;
 }
 
-void vampireGameproject::Character::setGeneration(std::string generation){
-    Character::generation = generation;
+void vampireGameproject::Character::setPlayername(std::string name){
+    playerName = name;
 }
 
-void vampireGameproject::Character::setAge(int age){
-    Character::age = age;
-}
-
-std::string vampireGameproject::Character::getNature(void){}
-char vampireGameproject::Character::getPlayername(void){}
-std::string vampireGameproject::Character::getChronicle(void){}
-int vampireGameproject::Character::getAge(void){}
-std::string vampireGameproject::Character::getDemeanor(void){}
-std::string vampireGameproject::Character::getClan(void){}
-std::string vampireGameproject::Character::getGeneration(void){}
