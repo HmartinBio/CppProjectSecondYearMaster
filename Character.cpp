9c39bc909539a7c126a25a0d404e4c7023365c85
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-09-29 19:17:49 +0200

    Modifing source code of Character class

--- Character.cpp
+++ Character.cpp
@@ -1,15 +1,64 @@
 #include "classes.h"
 
-vampireGameproject::Character::Character(Parsetext& textParser){
-    characterDaemenor()
-    
-}
+/** Constructor of the class Character.
+*
+* Modification of the Constructor of Character
+* for this one gives the Parsetext reference to 
+* the other class members contained in Character 
+*
+* @param textParser reference of Parsetext
+*/
+
+
+vampireGameproject::Character::Character(Parsetext& textParser)
+    : characterDaemenor(textParser), characterClan(textParser), 
+    characterHealth(textParser), characterNature(textParser), 
+    characterChronicle(textParser), characterGeneration(textParser){}
+
+
+
+/** Constructor of the class Character.
+*
+* Modification of the Constructor of Character
+* for this one gives the Parsetext reference to 
+* the other class members contained in Character 
+*
+* @param textParser reference of Parsetext
+*/
+
+
 
 vampireGameproject::Character::~Character(){}
+
+
+
+
+/** Constructor of the class Character.
+*
+* Modification of the Constructor of Character
+* for this one gives the Parsetext reference to 
+* the other class members contained in Character 
+*
+* @param textParser reference of Parsetext
+*/
+
+
 
 std::string vampireGameproject::Character::getPlayername(void){
     return playerName;
 }
+
+
+
+/** Constructor of the class Character.
+*
+* Modification of the Constructor of Character
+* for this one gives the Parsetext reference to 
+* the other class members contained in Character 
+*
+* @param textParser reference of Parsetext
+*/
+
 
 void vampireGameproject::Character::setPlayername(std::string name){
     playerName = name;
