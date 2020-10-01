a1de878f280c5282c79f4af2d9e968e3d210162a
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-10-01 10:14:03 +0200

    Modifing source code of Controllor class

---
+++ Controllor.cpp
@@ -0,0 +1,23 @@
+#include "classes.h"
+
+
+/** Constructor of the class Controllor.
+*
+* Modification of the constructor of Controllor
+* 
+*
+*
+*/
+
+vampireGameproject::Controllor::Controllor(){}
+
+
+/** Destructor of the class Controllor.
+*
+* Modification of the destructor of Controllor
+*
+*
+*
+*/
+
+vampireGameproject::Controllor::~Controllor(){}
