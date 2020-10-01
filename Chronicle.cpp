3ad487b346297eabab59404624e0e6aab40c061e
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-10-01 09:07:30 +0200

    Modifing source code of Chronicle class

--- Chronicle.cpp
+++ Chronicle.cpp
@@ -1,15 +1,77 @@
 #include "classes.h"
 
+
+
+/** Constructor of the class Chronicle.
+*
+* Modification of the constructor of Chronicle
+* Initialisation of a Parsetext reference 
+* before the Nature constructor be initialised
+*
+* @param textParser reference of Parsetext object
+*
+*/
+
+
+
 vampireGameproject::Chronicle::Chronicle(Parsetext& textParser): textParser(textParser){}
+
+
+
+/** Destructor of the class Chronicle.
+*
+* Modification of the destructor of Chronicle
+*
+*
+*
+*/
+
+
 vampireGameproject::Chronicle::~Chronicle(){}
+
+
+
+/** Implementation of the getChronicle method.
+*
+* Implementation of the getChronicle method
+* 
+*
+* 
+*/
+
+
 
 std::string vampireGameproject::Chronicle::getChronicle(void){
     return chronicle;
 }
 
+
+
+/** Implementation of the setChronicle method.
+*
+* Implementation of the setChronicle method
+* 
+* @param chronicle nature categorie to set
+* 
+*/
+
+
+
 void vampireGameproject::Chronicle::setChronicle(std::string chronicle){
     Chronicle::chronicle = chronicle;
 }
+
+
+
+/** Implementation of the setDescription method.
+*
+* Implementation of the setDescription method
+* 
+*
+* 
+*/
+
+
 
 void vampireGameproject::Chronicle::setDescription(void){
     description = textParser.parse_text();
