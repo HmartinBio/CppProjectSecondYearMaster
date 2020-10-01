064931d010a938ef2d439710da05d9c4e836e10e
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-10-01 10:14:36 +0200

    Modifing source code of Gui class

---
+++ Gui.cpp
@@ -0,0 +1,23 @@
+#include "classes.h"
+
+
+/** Constructor of the class Gui.
+*
+* Modification of the constructor of Gui
+* 
+*
+*
+*/
+
+vampireGameproject::Gui::Gui(): testGui(*this){}
+
+
+/** Destructor of the class Gui.
+*
+* Modification of the destructor of Gui
+*
+*
+*
+*/
+
+vampireGameproject::Gui::~Gui(){}
