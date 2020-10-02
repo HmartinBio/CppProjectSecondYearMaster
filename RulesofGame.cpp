1b5ed8c4e05d63c821be29f9049880312c77eed1
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-10-02 23:39:39 +0200

    Adding source code of RulesofGame class

---
+++ RulesofGame.cpp
@@ -0,0 +1,26 @@
+#include "classes.h"
+
+
+/** Constructor of the class RulesofGame.
+*
+* Modification of the constructor of RulesofGame
+* Initialisation of a Gui reference 
+* before the Nature constructor be initialised
+*
+* @param classReference reference of the Gui class containing RulesofGame class
+*/
+
+vampireGameproject::RulesofGame::RulesofGame(Gui& classReference): referenceOutterclass(classReference){}
+
+
+/** Destructor of the class RulesofGame.
+*
+* Modification of the destructor of RulesofGame
+*
+*
+*
+*/
+
+
+vampireGameproject::RulesofGame::~RulesofGame(){}
+
