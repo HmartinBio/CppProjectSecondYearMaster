ad8cac2def4ddcfc8d07ee10fb22595129740c12
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-10-02 23:38:59 +0200

    Adding source code of Parsetext class

---
+++ Parsetext.cpp
@@ -0,0 +1,48 @@
+#include "classes.h"
+
+
+/** Constructor of the class Parsetext.
+*
+* Modification of the constructor of Parsetext
+*
+*
+*
+*/
+
+vampireGameproject::Parsetext::Parsetext(){}
+
+
+
+/** Destructor of the class Parsetext.
+*
+* Modification of the destructor of Parsetext
+*
+*
+*
+*/
+
+
+vampireGameproject::Parsetext::~Parsetext(){}
+
+
+
+/** Implementation of the parse_text method.
+*
+* Implementation of the parse_text method
+* Method returning a parsed text corresponding to  
+* the description of the searched element 
+* 
+* @param categorie categorie name to parse on the file
+* 
+*/
+
+
+std::string vampireGameproject::Parsetext::parse_text(std::string categorie){}
+
+
+/**
+ # @overload std::string parse_text(std::string categorie, std::string table, std::string nameOfcomptence)
+ */
+
+
+std::string vampireGameproject::Parsetext::parse_text(std::string categorie, std::string table, std::string nameOfcomptence){}
