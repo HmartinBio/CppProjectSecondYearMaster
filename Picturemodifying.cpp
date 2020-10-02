1f9e10bee91e9819b9e16476f7379c5affdeb64c
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-10-02 23:39:16 +0200

    Adding source code of Picturemodifying class

---
+++ Picturemodifying.cpp
@@ -0,0 +1,23 @@
+#include "classes.h"
+
+
+/** Constructor of the class Picturemodifying.
+*
+* Modification of the constructor of Picturemodifying
+*
+*
+*
+*/
+
+vampireGameproject::Picturemodifying::Picturemodifying(){}
+
+
+/** Destructor of the class Picturemodifying.
+*
+* Modification of the destructor of Picturemodifying
+*
+*
+*
+*/
+
+vampireGameproject::Picturemodifying::~Picturemodifying(){}
