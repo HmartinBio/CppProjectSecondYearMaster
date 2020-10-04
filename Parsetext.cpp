a42fd74cfa0b7e34e62e84f18df3ddc186035e26
Hugo MARTIN <hugo.martin.1@etu.univ-amu.fr>
2020-10-04 19:40:54 +0200

    Modifing source code of Parsetext class

--- Parsetext.cpp
+++ Parsetext.cpp
@@ -36,22 +36,32 @@ vampireGameproject::Parsetext::~Parsetext(){}
 * 
 */
 
+// Allows to parse all the text of a categorie (Abilities, Attributes,...)
 
 std::string vampireGameproject::Parsetext::parse_text(std::string categorie){
     std::string lineOftext = "";
-    std::string partOftext;
     std::ifstream infile("VampireV5.txt");
-    std::string lineTofind = "#### " + categorie;
+
+    // while we don't find the searched categorie
+    // beginning by "####" we take the next line
     
-    while(lineOftext != lineTofind){
-        std::getline(infile, lineOftext);
+    while(pystring::startswith(lineOftext, "####") != true && 
+        pystring::find(lineOftext, categorie) == -1){
+            std::getline(infile, lineOftext);
     }
 
+    // we create a string to save a part of 
+    // the text we want 
+    
+    std::string partOftext;
     partOftext = categorie + "\n\n";
     std::getline(infile, lineOftext);
     partOftext += lineOftext;
     partOftext += "\n";
     
+    // while we don't reach the end of the paragraph
+    // we save text the in the string
+
     while(pystring::startswith(lineOftext, "#") != true){
         std::getline(infile, lineOftext);
         partOftext += lineOftext;
@@ -59,33 +69,49 @@ std::string vampireGameproject::Parsetext::parse_text(std::string categorie){
     
     infile.close();
     }
+    return partOftext;
 }
 
 /**
  # @overload std::string parse_text(std::string categorie, std::string caracteristics)
  */
 
+// Allows to parse Clans
+
 std::string vampireGameproject::Parsetext::parse_text(std::string categorie, std::string caracteristics){
     std::string lineOftext = "";
     std::string partOftext;
     std::ifstream infile("VampireV5.txt");
-    std::string lineTofind = "#### " + categorie;
+    std::string lineTofind;
     
-    while(lineOftext != lineTofind){
+    // while we don't find the searched categorie
+    // beginning by "####" we take the next line
+
+    while(pystring::startswith(lineOftext, "####") != true && 
+        pystring::find(lineOftext, categorie) == -1){
+            std::getline(infile, lineOftext);
+    }
+
+    lineTofind = "--" + caracteristics;
+
+    // while we don't find the caracteristics 
+    // we take the next line of the file 
+
+    while(pystring::startswith(lineOftext, lineTofind) != true){
         std::getline(infile, lineOftext);
     }
 
-    std::string lineTofind = "--" + caracteristics;
-
-    while(lineOftext != lineTofind){
-        std::getline(infile, lineOftext);
-    }
+    // we create a string to save a part of 
+    // the text we want 
 
     partOftext = caracteristics + "\n\n";
     std::getline(infile, lineOftext);
     partOftext += lineOftext;
     partOftext += "\n";
-    
+
+    // while we don't reach the end of the paragraph
+    // we save text the in the string    
+
     while(pystring::startswith(lineOftext, "--") != true){
         std::getline(infile, lineOftext);
         partOftext += lineOftext;
@@ -93,6 +119,7 @@ std::string vampireGameproject::Parsetext::parse_text(std::string categorie, std
     
     infile.close();
     }
+    return partOftext;
 }
 
 
@@ -100,6 +127,7 @@ std::string vampireGameproject::Parsetext::parse_text(std::string categorie, std
  # @overload std::string parse_text(std::string categorie, std::string table, std::string nameOfcomptence)
  */
 
+// allows to parse disciplines and merits_flaws
 
 std::string vampireGameproject::Parsetext::parse_text(std::string categorie, std::string table, std::string nameOfcomptence){
     
@@ -107,13 +135,136 @@ std::string vampireGameproject::Parsetext::parse_text(std::string categorie, std
     std::string lineOftext = "";
     std::string partOftext;
     std::ifstream infile("VampireV5.txt");
-    std::string lineTofind = "#### " + categorie;
+
+    // while we don't find the searched categorie
+    // beginning by "####" we take the next line
+
     
-    while(lineOftext != lineTofind){
-        std::getline(infile, lineOftext);
+    while(pystring::startswith(lineOftext, "####") != true && 
+        pystring::find(lineOftext, categorie) == -1){
+            std::getline(infile, lineOftext);
     }
 
+
     if(table == "Merits_flaws"){
+
+        // while we don't find the searched table
+        // beginning by "##" we take the next line
+
+        while(pystring::startswith(lineOftext, "##") != true && 
+            pystring::find(lineOftext, pystring::capitalize(table)) == -1){
+                std::getline(infile, lineOftext);
+            }
+        
+        // while we don't find the searched nameofCompetence
+        // beginning by "--" we take the next line
+
+        while(pystring::startswith(lineOftext, "--") != true && 
+            pystring::find(lineOftext, nameOfcomptence) == -1){
+                std::getline(infile, lineOftext);
+            }
+        
+        // we create a string to save a part of 
+        // the text we want
+
+        partOftext = nameOfcomptence + "\n\n";
+        std::getline(infile, lineOftext);
+        partOftext += lineOftext;
+        partOftext += "\n";
+
+        // while we don't reach the end of the paragraph
+        // we save text the in the string 
+
+        while(pystring::startswith(lineOftext, "--") != true){
+            std::getline(infile, lineOftext);
+            partOftext += lineOftext;
+            partOftext += "\n";
+        
+        }
+        
+        infile.close();
+        return partOftext;
+    }
+
+    else if(table == "Disciplines"){
+        
+        // while we don't find the searched table
+        // beginning by "--" we take the next line
+
+        while(pystring::startswith(lineOftext, "--") != true && 
+            pystring::find(lineOftext, pystring::capitalize(table)) == -1){
+                std::getline(infile, lineOftext);
+        }
+        
+        // We create two strings to search in the line 
+        // of text
+
+        std::string stringTofind1 = ":" + nameOfcomptence;
+        std::string stringTofind2 = ": " + nameOfcomptence;
+
+        // while we don't find the searched nameofCompetence
+        // beginning by "\t [0-9]:" we take the next line
+
+        while(pystring::startswith(lineOftext, "\t") != true &&
+            pystring::find(lineOftext, stringTofind1) != -1 || 
+            pystring::find(lineOftext, stringTofind2) != -1   
+            ){
+                std::getline(infile, lineOftext);
+            }
+
+        // we create a string to save a part of 
+        // the text we want
+
+        partOftext = nameOfcomptence + "\n\n";
+        std::getline(infile, lineOftext);
+        partOftext += lineOftext;
+        partOftext += "\n";
+
+        // we create a bool to test if we are on the next paragraph
+
+        bool testLine = std::regex_search(lineOftext, std::regex("^\t[0-9]:"));
+
+        // while we don't reach the next paragraph
+        // we save in a string the text we want
+
+        while(testLine == false){
+            std::getline(infile, lineOftext);
+            partOftext += lineOftext;
+            partOftext += "\n";
+            testLine = std::regex_search(lineOftext,
+                std::regex("^\t[0-9]:"));
+        }
+
+        infile.close();
+        return partOftext;
+
+    }
+
+
+    //while(lineOftext != lineTofind){
+    //    std::getline(infile, lineOftext);
+    //}
+
+    //if(table == "Merits_flaws"){
+    //    std::string lineTofind = "##" + table;
+    //}
+
+    //else if(table == "Disciplines"){
+    //    std::string lineTofind = "--" + table;
+    //}
+
+    //while(lineOftext != lineTofind){
+    //    std::getline(infile, lineOftext);
+    //}
+
+   /*  if(table == "Merits_flaws"){
+        while(pystring::startswith(lineOftext, "##") != true && 
+            pystring::find(lineOftext, pystring::capitalize()  
+            
+            while(pystring::find(lineOftext, "####") == - 1 && 
+        pystring::find(lineOftext, categorie) == -1){
+            std::getline(infile, lineOftext);
+    }) )
         std::string lineTofind = "##" + table;
     }
 
@@ -121,13 +272,7 @@ std::string vampireGameproject::Parsetext::parse_text(std::string categorie, std
         std::string lineTofind = "--" + table;
     }
 
-    
-
-    while(lineOftext != lineTofind){
-        std::getline(infile, lineOftext);
-    }
-
-    partOftext = caracteristics + "\n\n";
+    partOftext = nameOfcomptence + "\n\n";
     std::getline(infile, lineOftext);
     partOftext += lineOftext;
     partOftext += "\n";
@@ -139,4 +284,4 @@ std::string vampireGameproject::Parsetext::parse_text(std::string categorie, std
     
     infile.close();
     }
-}
+} */
