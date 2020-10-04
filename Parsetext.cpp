39c0f74ac6d58c1e7c2414292231bcb957356463
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-10-04 17:26:56 +0200

    Modfing source code of Parsetext class

--- Parsetext.cpp
+++ Parsetext.cpp
@@ -37,7 +37,63 @@ vampireGameproject::Parsetext::~Parsetext(){}
 */
 
 
-std::string vampireGameproject::Parsetext::parse_text(std::string categorie){}
+std::string vampireGameproject::Parsetext::parse_text(std::string categorie){
+    std::string lineOftext = "";
+    std::string partOftext;
+    std::ifstream infile("VampireV5.txt");
+    std::string lineTofind = "#### " + categorie;
+    
+    while(lineOftext != lineTofind){
+        std::getline(infile, lineOftext);
+    }
+
+    partOftext = categorie + "\n\n";
+    std::getline(infile, lineOftext);
+    partOftext += lineOftext;
+    partOftext += "\n";
+    
+    while(pystring::startswith(lineOftext, "#") != true){
+        std::getline(infile, lineOftext);
+        partOftext += lineOftext;
+        partOftext += "\n";
+    
+    infile.close();
+    }
+}
+
+/**
+ # @overload std::string parse_text(std::string categorie, std::string caracteristics)
+ */
+
+std::string vampireGameproject::Parsetext::parse_text(std::string categorie, std::string caracteristics){
+    std::string lineOftext = "";
+    std::string partOftext;
+    std::ifstream infile("VampireV5.txt");
+    std::string lineTofind = "#### " + categorie;
+    
+    while(lineOftext != lineTofind){
+        std::getline(infile, lineOftext);
+    }
+
+    std::string lineTofind = "--" + caracteristics;
+
+    while(lineOftext != lineTofind){
+        std::getline(infile, lineOftext);
+    }
+
+    partOftext = caracteristics + "\n\n";
+    std::getline(infile, lineOftext);
+    partOftext += lineOftext;
+    partOftext += "\n";
+    
+    while(pystring::startswith(lineOftext, "--") != true){
+        std::getline(infile, lineOftext);
+        partOftext += lineOftext;
+        partOftext += "\n";
+    
+    infile.close();
+    }
+}
 
 
 /**
@@ -45,4 +101,42 @@ std::string vampireGameproject::Parsetext::parse_text(std::string categorie){}
  */
 
 
-std::string vampireGameproject::Parsetext::parse_text(std::string categorie, std::string table, std::string nameOfcomptence){}
+std::string vampireGameproject::Parsetext::parse_text(std::string categorie, std::string table, std::string nameOfcomptence){
+    
+    
+    std::string lineOftext = "";
+    std::string partOftext;
+    std::ifstream infile("VampireV5.txt");
+    std::string lineTofind = "#### " + categorie;
+    
+    while(lineOftext != lineTofind){
+        std::getline(infile, lineOftext);
+    }
+
+    if(table == "Merits_flaws"){
+        std::string lineTofind = "##" + table;
+    }
+
+    else if(table == "Disciplines"){
+        std::string lineTofind = "--" + table;
+    }
+
+    
+
+    while(lineOftext != lineTofind){
+        std::getline(infile, lineOftext);
+    }
+
+    partOftext = caracteristics + "\n\n";
+    std::getline(infile, lineOftext);
+    partOftext += lineOftext;
+    partOftext += "\n";
+    
+    while(pystring::startswith(lineOftext, "--") != true){
+        std::getline(infile, lineOftext);
+        partOftext += lineOftext;
+        partOftext += "\n";
+    
+    infile.close();
+    }
+}
