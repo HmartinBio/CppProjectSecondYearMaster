cbb74800c6a10521bfc264b55ea28c2234ae2c59
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-10-14 16:41:25 +0200

    Modifing launcher source code

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+// Ne pas oublier de faire l'héritage de la Window
+// Ne pas oublier de regarder Weakness des clans car 
+// certains clans ne peuvent avoir certains Attributs et Disciplines
+
 int main(int argc, char* argv[]){
     auto app = Gtk::Application::create(argc, argv);
     vampireGameproject::Controllor appControlor;
