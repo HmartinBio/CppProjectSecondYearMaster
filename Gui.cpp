6364cce9c37c37ed8fc261c904c2eb17df830fc8
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-10-14 14:28:22 +0200

    Modifing source code Gui class

--- Gui.cpp
+++ Gui.cpp
@@ -10,7 +10,18 @@
 */
 
 vampireGameproject::Gui::Gui(RulesofGame& rulesGamereference): 
-    testGui(rulesGamereference){}
+    testGui(rulesGamereference){
+        window.set_title("Vampire Game Project");
+        window.resize(400, 300);
+        window.set_position(Gtk::WIN_POS_CENTER);
+        tableLabel[0] = Gtk::Label();
+        tableLabel[0].set_text("Select a clan");
+        mainGrid.attach(tableLabel[0], 0, 0, 1, 1);
+        mainGrid.show_all();
+        window.add(mainGrid);
+        window.show_all_children();
+    }
+
 
 
 /** Destructor of the class Gui.
