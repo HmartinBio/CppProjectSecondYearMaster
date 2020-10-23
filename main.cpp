323461394fbb22a6e16ae170f222894b37889299
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-10-24 00:42:38 +0200

    Modifing launcher code

--- main.cpp
+++ main.cpp
@@ -12,6 +12,11 @@ using namespace std;
 int main(int argc, char* argv[]){
     auto app = Gtk::Application::create(argc, argv);
     vampireGameproject::Controllor appControlor;
+    //vampireGameproject::Parsetext parser;
+    //vector<string> vecteur = parser.parse_ClanDisciplines("Nosferatu");
+    //for(int i = 0; i < (int)vecteur.size(); i++){
+    //    cout << vecteur.at(i) << endl;
+    //}
     app->run(appControlor.graphicalUserinterface) ;
     //cout << "test" << endl;
     return 0;
