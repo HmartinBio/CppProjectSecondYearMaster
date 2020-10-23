30f63dbcbd2b692c7f1423e241be8557b7a7c309
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-10-24 00:41:19 +0200

    Modifing source code of Controllor class

--- Controllor.cpp
+++ Controllor.cpp
@@ -10,7 +10,29 @@
 */
 
 vampireGameproject::Controllor::Controllor(): graphicalUserinterface(rulesGame),
-    rulesGame(graphicalUserinterface){}
+    rulesGame(textParser, graphicalUserinterface){
+        graphicalUserinterface.set_title("Vampire Game Project");
+        graphicalUserinterface.resize(400, 300);
+        graphicalUserinterface.set_position(Gtk::WIN_POS_CENTER);
+        graphicalUserinterface.initialiseTableLabel();
+        graphicalUserinterface.setTableLabelAlign(0, "Select a clan:");
+        graphicalUserinterface.setTableLabelAlign(1, "Select a Nature:");
+        graphicalUserinterface.setTableLabel(2, "    Name of the Vampire:");
+        graphicalUserinterface.setTableLabelAlign(3, "Select Attributes:");
+        graphicalUserinterface.setTableLabelOnGrid(0, 0, 0, 1, 1);
+        graphicalUserinterface.setTableLabelOnGrid(1, 0, 1, 1, 1);
+        graphicalUserinterface.setTableLabelOnGrid(2, 2, 0, 1, 1);
+        graphicalUserinterface.setTableLabelOnGrid(3, 0, 2, 1, 1);
+        graphicalUserinterface.initialiseTableComboBox();
+        rulesGame.setVectorClans();
+        rulesGame.setVectorNature();
+        graphicalUserinterface.setTableComboBox(0, rulesGame.returnVectorClans());
+        graphicalUserinterface.setTableComboBox(1, rulesGame.returnVectorNature());
+        graphicalUserinterface.setTableComboBoxOnGrid(0, 0);
+        graphicalUserinterface.setTableComboBoxOnGrid(1, 1);
+        graphicalUserinterface.setTableComboBoxOnGrid(2, 3);
+        graphicalUserinterface.showAll();
+    }
 
 
 /** Destructor of the class Controllor.
