c1d17dcdd541c2c92ce1f5ebe9e958e6cd5bc4b2
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-10-24 00:41:39 +0200

    Modifing source code of Gui class

--- Gui.cpp
+++ Gui.cpp
@@ -11,46 +11,52 @@
 
 vampireGameproject::Gui::Gui(RulesofGame& rulesGamereference): 
     testGui(rulesGamereference){
-        Gui::set_title("Vampire Game Project");
-        Gui::resize(400, 300);
-        Gui::set_position(Gtk::WIN_POS_CENTER);
+        //Gui::set_title("Vampire Game Project");
+        //Gui::resize(400, 300);
+        //Gui::set_position(Gtk::WIN_POS_CENTER);
         
-        for(int iterator = 0; iterator <= 3; iterator++){
-            tableLabel[iterator] = Gtk::Label();
-        }
+        //for(int iterator = 0; iterator <= 3; iterator++){
+        //    tableLabel[iterator] = Gtk::Label();
+        //}
 
-        tableLabel[0].set_text("Select a clan:");
-        tableLabel[0].set_xalign(0.0);
-        tableLabel[1].set_text("Select a Nature:");
-        tableLabel[1].set_xalign(0.0);
-        tableLabel[2].set_text("    Name of the Vampire:");
-        tableLabel[3].set_text("Select Attributes:");
-        tableLabel[3].set_xalign(0.0);
-        mainGrid.attach(tableLabel[0], 0, 0, 1, 1);
-        mainGrid.attach(tableLabel[1], 0, 1, 1, 1);
-        mainGrid.attach(tableLabel[2], 2, 0, 1, 1);
-        mainGrid.attach(tableLabel[3], 0, 2, 1, 1);
-        mainGrid.insert_row(1);
-        tableComboBox[0] = Gtk::ComboBoxText();
-        tableComboBox[1] = Gtk::ComboBoxText();
-        tableComboBox[0].append("Assamite");
-        tableComboBox[0].append("Brujah");
-        tableComboBox[1].append("Stealer");
-        //mainGrid.attach(tableComboBox[0], 1, 0, 1, 1);
-        mainGrid.attach_next_to(tableComboBox[0], tableLabel[0], Gtk::POS_RIGHT, 1, 1);
-        mainGrid.attach_next_to(tableComboBox[1], tableLabel[3], Gtk::POS_RIGHT, 1, 1);
-        //mainGrid.attach(tableComboBox[1], 1, 1, 1, 1);
-        //mainGrid.set_row_homogeneous(true);
-        //mainGrid.set_column_homogeneous(true);
-        mainGrid.show_all();
-        Gui::add(mainGrid);
-        Gui::show_all_children();
+        //tableLabel[0].set_text("Select a clan:");
+        //tableLabel[0].set_xalign(0.0);
+        //tableLabel[1].set_text("Select a Nature:");
+        //tableLabel[1].set_xalign(0.0);
+        //tableLabel[2].set_text("    Name of the Vampire:");
+        //tableLabel[3].set_text("Select Attributes:");
+        //tableLabel[3].set_xalign(0.0);
+        //mainGrid.attach(tableLabel[0], 0, 0, 1, 1);
+        //mainGrid.attach(tableLabel[1], 0, 1, 1, 1);
+        //mainGrid.attach(tableLabel[2], 2, 0, 1, 1);
+        //mainGrid.attach(tableLabel[3], 0, 2, 1, 1);
+        //mainGrid.insert_row(1);
+        //tableComboBox[0] = Gtk::ComboBoxText();
+        //tableComboBox[1] = Gtk::ComboBoxText();
+        
+        ////std::vector<std::string> vectorClans = testGui.returnVectorClans();
+
+        ////for(int iterator = 0; iterator < (int)vectorClans.size(); iterator++){
+        ////    tableComboBox[0].append(vectorClans.at(iterator));
+        ////}
+        
+        //tableComboBox[0].append("Assamite");
+        //tableComboBox[0].append("Brujah");
+        //tableComboBox[1].append("Stealer");
+        ////mainGrid.attach(tableComboBox[0], 1, 0, 1, 1);
+        //mainGrid.attach_next_to(tableComboBox[0], tableLabel[0], Gtk::POS_RIGHT, 1, 1);
+        //mainGrid.attach_next_to(tableComboBox[1], tableLabel[3], Gtk::POS_RIGHT, 1, 1);
+        ////mainGrid.attach(tableComboBox[1], 1, 1, 1, 1);
+        ////mainGrid.set_row_homogeneous(true);
+        ////mainGrid.set_column_homogeneous(true);
+        //mainGrid.show_all();
+        //Gui::add(mainGrid);
+        //Gui::show_all_children();
     }
 
 
 
 /** Destructor of the class Gui.
-*
 * Modification of the destructor of Gui
 *
 *
@@ -58,3 +64,44 @@ vampireGameproject::Gui::Gui(RulesofGame& rulesGamereference):
 */
 
 vampireGameproject::Gui::~Gui(){}
+
+void vampireGameproject::Gui::setTableComboBox(int index, std::vector<std::string> vectorElements){
+    for(int iterator = 0; iterator < (int)vectorElements.size(); iterator++){
+        tableComboBox[index].append(vectorElements.at(iterator));
+    }
+}
+
+void vampireGameproject::Gui::initialiseTableLabel(){
+    for(int iterator = 0; iterator <= 3; iterator++){
+            tableLabel[iterator] = Gtk::Label();
+        }
+}
+
+void vampireGameproject::Gui::initialiseTableComboBox(){
+    tableComboBox[0] = Gtk::ComboBoxText();
+    tableComboBox[1] = Gtk::ComboBoxText();
+    tableComboBox[2] = Gtk::ComboBoxText();
+}
+
+void vampireGameproject::Gui::setTableLabelAlign(int index, std::string label){
+    tableLabel[index].set_text(label);
+    tableLabel[index].set_xalign(0.0);
+}
+
+void vampireGameproject::Gui::setTableLabel(int index, std::string label){
+    tableLabel[index].set_text(label);
+}
+
+void vampireGameproject::Gui::setTableLabelOnGrid(int index, int xcoordinates, int ycoordinates, int width, int height){
+    mainGrid.attach(tableLabel[index], xcoordinates, ycoordinates, width, height);
+}
+
+void vampireGameproject::Gui::setTableComboBoxOnGrid(int index, int indexLabel){
+    mainGrid.attach_next_to(tableComboBox[index], tableLabel[indexLabel], Gtk::POS_RIGHT, 1, 1);
+}
+
+void vampireGameproject::Gui::showAll(){
+    mainGrid.show_all();
+    Gui::add(mainGrid);
+    Gui::show_all_children();
+}
