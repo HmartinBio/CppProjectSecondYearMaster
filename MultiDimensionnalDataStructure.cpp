d41933eb1736ef54d2edda8510832d0546348989
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-11-28 15:37:12 +0100

    Modifing MultiDimensionnalDataStructure class source code

--- MultiDimensionnalDataStructure.cpp
+++ MultiDimensionnalDataStructure.cpp
@@ -1,5 +1,41 @@
 #include "classes.h"
 
+
+/** Constructor of the class MultiDimensionnalDataStructure.
+*
+* Modification of the constructor of MultiDimensionnalDataStructure
+* Initialisation of a Character reference 
+* before the MultiDimensionnalDataStructure constructor be initialised
+* 
+* @param textParse: reference of Parsetext
+*/
+
+
 vampireGameproject::MultiDimensionnalDataStructure::MultiDimensionnalDataStructure(std::string categorieToEnter):categorie(categorieToEnter){}
+
+
+
+/**
+ # @overload vampireGameproject::MultiDimensionnalDataStructure::MultiDimensionnalDataStructure(int categorieValue)
+ */
+
+
+
 vampireGameproject::MultiDimensionnalDataStructure::MultiDimensionnalDataStructure(int categorieValue):value(categorieValue){}
+
+
+
+
+
+/** Destructor of the class MultiDimensionnalDataStructure.
+*
+* Modification of the Destructor of MultiDimensionnalDataStructure */
+
+
 vampireGameproject::MultiDimensionnalDataStructure::~MultiDimensionnalDataStructure(){}
+
+
+
+
+
+
