e06eee981ef34055c973b88fef178732922f41b7
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-11-29 17:01:00 +0100

    Modifing Abilities class source code

--- Abilities.cpp
+++ Abilities.cpp
@@ -1,3 +1,16 @@
+/**
+ * @file Abilities.cpp
+ *
+ * @brief File containing source code of the Abilities class
+ *
+ *
+ * @author Hugo MARTIN
+ * Contact: hugo.martin.1@etu.univ-amu.fr
+ *
+ */
+
+
+
 #include "classes.h"
 
 
