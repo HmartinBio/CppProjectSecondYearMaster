f4dd35b161d31660dea4306291d36706cd68b7a7
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-11-29 17:01:16 +0100

    Modifing Advantages class source code

--- Advantages.cpp
+++ Advantages.cpp
@@ -1,3 +1,16 @@
+/**
+ * @file Advantages.cpp
+ *
+ * @brief File containing source code of the Advantages class
+ *
+ *
+ * @author Hugo MARTIN
+ * Contact: hugo.martin.1@etu.univ-amu.fr
+ *
+ */
+
+
+
 #include "classes.h"
 
 
