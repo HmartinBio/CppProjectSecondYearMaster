cf17f233633da39a7338390b597cc428c6a99bd5
m16016813 <hugo.martin.1@etu.univ-amu.fr>
2020-11-29 17:05:32 +0100

    Modifing MultiInput class source code

--- MultiInput.cpp
+++ MultiInput.cpp
@@ -1,3 +1,16 @@
+/**
+ * @file MultiInput.cpp
+ *
+ * @brief File containing source code of the MultiInput class
+ *
+ *
+ * @author Hugo MARTIN
+ * Contact: hugo.martin.1@etu.univ-amu.fr
+ *
+ */
+
+
+
 #include "classes.h"
 
 
